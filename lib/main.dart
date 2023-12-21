674fa1b099aa51351760f8956258671878f9ddbc
Mohamed Abdelmaboud <mohamedabdelmabod03@gmail.com>
2023-12-21 14:22:24 +0200

    add my bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shop/cubits/cart_cubit/cart_cubit.dart';
 import 'package:shop/cubits/categories_cubit/categories_cubit.dart';
 import 'package:shop/cubits/products_cubit/products_cubit.dart';
+import 'package:shop/firebase_options.dart';
+import 'package:shop/my_boc_observer.dart';
 import 'package:shop/views/cart_view.dart';
 import 'package:shop/views/details_view.dart';
 import 'package:shop/views/home_view.dart';
@@ -11,7 +14,12 @@ import 'package:shop/views/signUp_view.dart';
 import 'views/login_view.dart';
 import 'views/onboarding_view.dart';
 
-void main() {
+void main() async {
+  Bloc.observer = MyBlocObserver();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -26,8 +34,12 @@ class MyApp extends StatelessWidget {
         BlocProvider(
           create: (context) => ProductsCubit(),
         ),
-        BlocProvider(create: (context) => CategoriesCubit(),)
-        ,BlocProvider(create: (context) => CartCubit(),)
+        BlocProvider(
+          create: (context) => CategoriesCubit(),
+        ),
+        BlocProvider(
+          create: (context) => CartCubit(),
+        )
       ],
       child: MaterialApp(
         theme: ThemeData(
@@ -42,7 +54,7 @@ class MyApp extends StatelessWidget {
           SignUpView.id: (context) => const SignUpView(),
           LoginView.id: (context) => const LoginView(),
           OnboardingView.id: (context) => const OnboardingView(),
-          CartView.id:(context) => const CartView()
+          CartView.id: (context) => const CartView()
         },
       ),
     );
---
+++ lib/services/sign_up.dart
@@ -0,0 +1,28 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:shop/widgets/show_snack_bar.dart';
+
+Future<UserCredential?> signUp(
+  String email,
+  String password,
+  BuildContext context,
+) async {
+  try {
+    FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+    final UserCredential userCredential = await firebaseAuth
+        .createUserWithEmailAndPassword(email: email, password: password);
+    return userCredential;
+  } on FirebaseAuthException catch (e) {
+    if (e.code == 'weak-password') {
+      showSnackBar(context, 'The password provided is too weak.', true);
+    } else if (e.code == 'email-already-in-use') {
+      showSnackBar(context, 'The account already exists for that email.', true);
+    }
+    return null;
+  } catch (e) {
+    print(e);
+  }
+  return null;
+}
